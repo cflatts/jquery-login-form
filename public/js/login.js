bd1b741bbef113797a85be7b76a4a10147548162
Caitlin Flattery <caitlinflattery@gmail.com>
2017-04-12 16:08:07 -0500

    added new font, added error handlers where text displays what happened

--- public/js/login.js
+++ public/js/login.js
@@ -6,10 +6,28 @@ function loginSuccess (response) {
   window.location = 'http://127.0.0.1:7979/index.html'
 }
 
+function createErrorEl (newEl, errorText, appendEl) {
+  $(newEl, {
+    html: errorText,
+    'class': 'error'
+  }).appendTo(appendEl)
+}
+
 function loginFail (response, error, status) {
-  console.log('ERROR RESPONSE:', response)
-  console.log('ERROR:', error)
-  console.log('ERROR STATUS:', status)
+  console.log(response)
+  if (response.readyState === 0) {
+    createErrorEl('<p/>', 'The server has timed out', '.action-row')
+  } else if (response.readyState === 4) {
+    if (response.status === 400) {
+      if (response.responseText === '{"error":"Invalid username."}') {
+        createErrorEl('<p/>', 'The username is incorrect', '.username')
+      } else if (response.responseText === '{"error":"Invalid password."}') {
+        createErrorEl('<p/>', 'The password is incorrect', '.password')
+      }
+    } else if (response.status === 500) {
+      createErrorEl('<p/>', 'The server has responded with an error', '.action-row')
+    }
+  }
 }
 
 function login (evt) {
