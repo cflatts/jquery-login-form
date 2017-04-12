0c2aa71dc1c86c38df8951c043cb9ed508d1c218
Caitlin Flattery <caitlinflattery@gmail.com>
2017-04-12 13:49:31 -0500

    created post request for logging in, not handling erros yet

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,32 @@
 /* global $ */
 
-console.log($)
+var loginBtn = $('#loginBtn')
+
+function loginSuccess (response) {
+  console.log('SUCCESS:', response)
+}
+
+function loginFail (response, error, status) {
+  console.log('ERROR RESPONSE:', response)
+  console.log('ERROR:', error)
+  console.log('ERROR STATUS:', status)
+}
+
+function login (evt) {
+  evt.preventDefault()
+  var username = $('#loginInput')
+  var password = $('#passwordInput')
+
+  $.ajax({
+    method: 'POST',
+    data: {
+      username: username.val(),
+      password: password.val()
+    },
+    url: 'http://127.0.0.1:7979/api/login',
+    success: loginSuccess,
+    error: loginFail,
+    timeout: 15000
+  })
+}
+loginBtn.click(login)
